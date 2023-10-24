b9d0d9b7d02e69e8b7fce00ab322a1b266de4e22
muhila16 <muhila.s2021@vitstudent.ac.in>
2023-10-24 21:40:55 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// App.js 
+import React from "react"; 
+import WordLetterCounter from "./WordLetterCounter"; 
+import "./App.css"; 
+
+function App() { 
+	return ( 
+		<div className="App"> 
+			<h1 id="top"> 
+			
+			</h1> 
+			<h1> 
+			Words Counter 
+			</h1> 
+			<WordLetterCounter /> 
+		</div> 
+	); 
+} 
+
+export default App; 
